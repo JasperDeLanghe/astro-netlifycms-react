00cdd5285df2b1411d35914c60941da99735f2bd
Jasper De Langhe <jasper.delanghe@gmail.com>
2022-12-15 20:55:17 +0100

    broken astro-netlify-cms integration due to react()

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,34 @@ import { defineConfig } from "astro/config";
 import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: "git-gateway",
-          branch: "main",
-        },
-        collections: [
-          // Define a blog post collection
-          {
-            name: "posts",
-            label: "Blog Posts",
-            folder: "src/pages/posts",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              { name: "body", widget: "markdown", label: "Post Body" },
-            ],
-          },
-        ],
+  integrations: [NetlifyCMS({
+    config: {
+      backend: {
+        name: "git-gateway",
+        branch: "main"
       },
-    }),
-  ],
+      collections: [
+      // Define a blog post collection
+      {
+        name: "posts",
+        label: "Blog Posts",
+        folder: "src/pages/posts",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "title",
+          widget: "string",
+          label: "Post Title"
+        }, {
+          name: "body",
+          widget: "markdown",
+          label: "Post Body"
+        }]
+      }]
+    }
+  }), react()]
 });
